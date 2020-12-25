12fbba4f363c1d406d80015f71a9305e21341be1
Yoshio <yoshiokiyamusu@gmail.com>
2020-12-25 12:54:09 -0500

    comment

--- src/database.js
+++ src/database.js
@@ -51,35 +51,4 @@ api_tol_wms
 
 
 
-//PARA CONECTAR AL MYSQL REMOTO::
-const mysqlConnection = mysql.createConnection({
-        host: 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'cfvc0xg9mms2s789',
-        password: 'zcyp2fux4q42n6rm',
-        database: 'm6ul1nyhger85ik5',
-  multipleStatements: true
-});
-
-
-********************************
-CREATE DATABASE IF NOT EXISTS company;
-
-USE company;
-
-CREATE TABLE employee (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(45) DEFAULT NULL,
-  salary INT(11) DEFAULT NULL,
-  PRIMARY KEY(id)
-);
-
-DESCRIBE employee;
-
-INSERT INTO employee values
-  (1, 'Ryan Ray', 20000),
-  (2, 'Joe McMillan', 40000),
-  (3, 'John Carter', 50000);
-
-SELECT * FROM employee;
-
 */
