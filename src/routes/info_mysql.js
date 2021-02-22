b4bd3141beeaa37838a11ff3565d3a4f6d07b4be
Yoshio <yoshiokiyamusu@gmail.com>
2021-02-21 19:09:32 -0500

    maria db

--- src/routes/info_mysql.js
+++ src/routes/info_mysql.js
@@ -808,9 +808,9 @@ router.get("/powerbipruebaconclave",  isAuth, (req, res) => {
   };
   //console.log(data.proveedor);
 
-  $var_sql = "SELECT distinct os.orden_servicio, os.proveedor, DATE(os.fecha_envio) FROM orden_de_servicio ";
+  $var_sql = "SELECT distinct os.orden_servicio, os.proveedor, DATE(os.fecha_envio) as 'fecha_envio' FROM orden_de_servicio ";
   $var_sql += "AS os LEFT JOIN enviados_a_servicio AS es ON os.orden_servicio = es.orden_servicio ";
-  console.log($var_sql);
+  //console.log($var_sql);
 
   mysqlConnection.query($var_sql, (err, rows, fields) => {
     if (!err) {
